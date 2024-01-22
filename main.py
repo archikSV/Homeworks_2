a2f1c680c2b754bff961a523099d7830e34da238
SalchukArtem <archik.sv@gmail.com>
2024-01-22 08:54:02 +0200

    Task 2 completed.

--- main.py
+++ main.py
@@ -1,52 +1,60 @@
 print("Task date: 18.12.2023")
-print("Task: 1")
+print("Task: 2")
 
 """
-При старті додатку запускаються три потоки. Перший
-потік заповнює список випадковими числами. Два інші потоки
-очікують на заповнення. Коли перелік заповнений, обидва
-потоки запускаються. Перший потік знаходить суму елементів
-списку, другий потік знаходить середнє арифметичне значення
-у списку. Отриманий список, сума та середнє арифметичне
-виводяться на екран. 
+Користувач вводить з клавіатури шлях до файлу. Після
+чого запускаються три потоки. Перший потік заповнює файл
+випадковими числами. Два інші потоки очікують на заповнення. Коли файл заповнений, обидва потоки стартують.
+Перший потік знаходить усі прості числа, другий потік знаходить факторіал кожного числа у файлі. Результати пошуку
+кожен потік має записати у новий файл. Виведіть на екран
+статистику виконаних операцій.
 """
 
 import threading
 import random
+import math
 
-# Функція для заповнення списку випадковими числами
-def fill_list(lst):
-    for _ in range(10):
-        lst.append(random.randint(1, 100))
 
-# Функція для знаходження суми елементів списку
-def calculate_sum(lst):
-    total_sum = sum(lst)
-    print(f"Сума елементів списку: {total_sum}")
+def fill_file_with_random_numbers(file_path):
+    with open(file_path, 'w') as file:
+        for _ in range(10):
+            file.write(str(random.randint(1, 100)) + '\n')
 
-# Функція для знаходження середнього арифметичного значення у списку
-def calculate_average(lst):
-    avg = sum(lst) / len(lst)
-    print(f"Середнє арифметичне значення у списку: {avg}")
 
-# Створення пустого списку
-my_list = []
+def find_prime_numbers(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as file:
+        numbers = [int(line) for line in file.readlines()]
 
-# Створення потоків
-thread1 = threading.Thread(target=fill_list, args=(my_list,))
-thread2 = threading.Thread(target=calculate_sum, args=(my_list,))
-thread3 = threading.Thread(target=calculate_average, args=(my_list,))
+    prime_numbers = [num for num in numbers if all(num % i != 0 for i in range(2, int(math.sqrt(num)) + 1))]
 
-# Запуск першого потоку для заповнення списку
-thread1.start()
+    with open(output_file_path, 'w') as file:
+        file.write('\n'.join(map(str, prime_numbers)))
 
-# Очікування завершення заповнення списку
-thread1.join()
 
-# Запуск двох інших потоків
-thread2.start()
-thread3.start()
+def calculate_factorial(input_file_path, output_file_path):
+    with open(input_file_path, 'r') as file:
+        numbers = [int(line) for line in file.readlines()]
 
-# Очікування завершення обох потоків
-thread2.join()
-thread3.join()
+    factorials = [math.factorial(num) for num in numbers]
+
+    with open(output_file_path, 'w') as file:
+        file.write('\n'.join(map(str, factorials)))
+
+
+def main():
+    file_path = input("Enter the file path: ")
+    thread1 = threading.Thread(target=fill_file_with_random_numbers, args=(file_path,))
+    thread2 = threading.Thread(target=find_prime_numbers, args=(file_path, 'prime_numbers.txt'))
+    thread3 = threading.Thread(target=calculate_factorial, args=(file_path, 'factorials.txt'))
+
+    thread1.start()
+    thread2.start()
+    thread3.start()
+
+    thread1.join()
+    thread2.join()
+    thread3.join()
+
+
+if __name__ == "__main__":
+    main()
