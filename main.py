b576cb38d5a2e19653c011abe44e82f4c04003aa
SalchukArtem <archik.sv@gmail.com>
2024-01-22 07:25:40 +0200

    Task 1 completed.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-print("Task date: XX.XX.XXXX")
-print("Task: X")
+print("Task date: 22.11.2023")
+print("Task: 1")
 
 """
-
+ Метаклас, який вносить додаткові перевірки/логіку
+до певних методів у всіх класах.
 """
 
+class MyMeta(type):
+    def __new__(cls, name, bases, dct):
+        return super().__new__(cls, name, bases, dct)
+
+class MyClass(metaclass=MyMeta):
+    pass
