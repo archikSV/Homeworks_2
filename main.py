9a41de6414122455ec5c836db1f4f9fc4ca7f3a0
SalchukArtem <archik.sv@gmail.com>
2024-01-22 08:42:51 +0200

    Task 1 completed.

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-print("Task date: XX.XX.XXXX")
-print("Task: X")
+print("Task date: 13.12.2023")
+print("Task: 1")
 
 """
-
+Створіть два окремих "мікросервіси" (дві окремі
+програми). Одна програма створює та експортує дані у
+форматі JSON, а інша програма завантажує та обробляє ці
+дані. Це може бути, наприклад, система, яка створює та
+обробляє замовлення.
 """
 
+# Service 1: Export data in JSON format
+import json
+
+data = {
+    "order_id": 12345,
+    "product": "Smartphone",
+    "quantity": 2
+}
+
+with open('data.json', 'w') as file:
+    json.dump(data, file)
+
+
+# Service 2: Load and process the data
+import json
+
+with open('data.json', 'r') as file:
+    data = json.load(file)
+    print("Order ID:", data['order_id'])
+    print("Product:", data['product'])
+    print("Quantity:", data['quantity'])
