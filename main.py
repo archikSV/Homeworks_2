434aa9a332f2132915ac33f6c0ea8c9b712856e6
SalchukArtem <archik.sv@gmail.com>
2024-01-22 07:34:51 +0200

    Task 2 completed.

--- main.py
+++ main.py
@@ -1,59 +1,34 @@
 print("Task date: 29.11.2023")
-print("Task: 1")
+print("Task: 2")
 
 """
-Реалізуйте клас стеку для роботи з рядками (стек рядків).
-Стек має бути фіксованого розміру. Реалізуйте набір операцій
-для роботи зі стеком:
-o розміщення рядка у стек;
-o виштовхування рядка зі стеку;
-o підрахунок кількості рядків у стеку;
-o перевірку, чи порожній стек;
-o перевірку, чи повний стек;
-o очищення стеку;
-o отримання значення без виштовхування
-верхнього рядка зі стеку.
-На старті додатка відобразіть меню, в якому користувач
-може вибрати необхідну операцію
+Змініть стек із першого завдання таким чином, щоб його
+розмір був нефіксованим.
 """
 
-class FixedSizeStack:
-    def __init__(self, size):
-        self.stack = [None] * size
-        self.size = size
-        self.top = -1
+class Stack:
+    def __init__(self):
+        self.stack = []
 
     def push(self, item):
-        if self.top == self.size - 1:
-            print("Stack is full")
-        else:
-            self.top += 1
-            self.stack[self.top] = item
+        self.stack.append(item)
 
     def pop(self):
-        if self.top == -1:
+        if len(self.stack) == 0:
             print("Stack is empty")
+            return None
         else:
-            item = self.stack[self.top]
-            self.stack[self.top] = None
-            self.top -= 1
-            return item
+            return self.stack.pop()
 
     def count(self):
-        return self.top + 1
+        return len(self.stack)
 
     def is_empty(self):
-        return self.top == -1
-
-    def is_full(self):
-        return self.top == self.size - 1
-
-    def clear(self):
-        self.stack = [None] * self.size
-        self.top = -1
+        return len(self.stack) == 0
 
     def peek(self):
-        if self.top == -1:
+        if len(self.stack) == 0:
             print("Stack is empty")
+            return None
         else:
-            return self.stack[self.top]
+            return self.stack[-1]
