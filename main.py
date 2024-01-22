a9ecb94fc3d06d9b97b7d248ad959f203c4b092a
SalchukArtem <archik.sv@gmail.com>
2024-01-22 07:27:20 +0200

    Task 2 completed.

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 print("Task date: 22.11.2023")
-print("Task: 1")
+print("Task: 2")
 
 """
- Метаклас, який вносить додаткові перевірки/логіку
-до певних методів у всіх класах.
+Метаклас, що може змінювати ім'я класу залежно
+від певних умов або параметрів.
 """
 
-class MyMeta(type):
+class DynamicClassName(type):
     def __new__(cls, name, bases, dct):
-        return super().__new__(cls, name, bases, dct)
-
-class MyClass(metaclass=MyMeta):
-    pass
+       return super().__new__(cls, name, bases, dct)
