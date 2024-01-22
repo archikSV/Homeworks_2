48eed68ff6c8f59f0ecabbbb647cf34fb15e043c
SalchukArtem <archik.sv@gmail.com>
2024-01-22 07:28:50 +0200

    Task 4 completed.

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 print("Task date: 22.11.2023")
-print("Task: 3")
+print("Task: 4")
 
 """
-Створіть метаклас, який автоматично додає певні
-атрибути до всіх класів, що використовують його
+Метаклас, що додає перевірку та обробку аргументів
+__init__ у всіх класах.
 """
 
-class AutoAttributeAdder(type):
-    def __new__(cls, name, bases, dct):
-        dct['added_attribute'] = 'This attribute was added automatically'
-        return super().__new__(cls, name, bases, dct)
+class ArgumentCheckerMeta(type):
+    def __init__(cls, name, bases, dct):
+        original_init = cls.__init__
+
+        def new_init(self, *args, **kwargs):
+            print("Argument checking and processing logic here")
+            original_init(self, *args, **kwargs)
+
+        cls.__init__ = new_init
+        super().__init__(name, bases, dct)
